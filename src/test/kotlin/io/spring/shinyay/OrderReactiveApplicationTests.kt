22fe98a5b5122020a61e1ff5bb0e59f837c2006e
shinyay <shinya.com@gmail.com>
2021-10-30 10:24:43 +0900

    Initial Commit

---
+++ src/test/kotlin/io/spring/shinyay/OrderReactiveApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.spring.shinyay
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrderReactiveApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
