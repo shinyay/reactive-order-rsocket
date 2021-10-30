481b6cd14b64181d28c7d089f9ea1cced38f2ddf
shinyay <shinya.com@gmail.com>
2021-10-30 10:54:04 +0900

    Add OrderDataStore with ConcurrentHashMap

--- src/main/kotlin/io/spring/shinyay/controller/OrderController.kt
+++ src/main/kotlin/io/spring/shinyay/controller/OrderController.kt
@@ -1,4 +1,18 @@
 package io.spring.shinyay.controller
 
+import io.spring.shinyay.entity.Order
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.ResponseBody
+import java.util.concurrent.ConcurrentHashMap
+
+
+
+
+@Controller
+@ResponseBody
 class OrderController {
+
+    val orderDataStore: Map<Int, Collection<Order>> = ConcurrentHashMap()
+
+
 }
