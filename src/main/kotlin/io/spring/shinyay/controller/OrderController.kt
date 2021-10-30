5db1f3a09935e8966ccf19ac449873e60c0d02b8
shinyay <shinya.com@gmail.com>
2021-10-30 11:10:31 +0900

    Add RSocket Endpoint

--- src/main/kotlin/io/spring/shinyay/controller/OrderController.kt
+++ src/main/kotlin/io/spring/shinyay/controller/OrderController.kt
@@ -1,8 +1,11 @@
 package io.spring.shinyay.controller
 
 import io.spring.shinyay.entity.Order
+import org.springframework.messaging.handler.annotation.DestinationVariable
+import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.ResponseBody
+import reactor.core.publisher.Flux
 import java.util.concurrent.ConcurrentHashMap
 
 
@@ -12,7 +15,11 @@ import java.util.concurrent.ConcurrentHashMap
 @ResponseBody
 class OrderController {
 
-    val orderDataStore: Map<Int, Collection<Order>> = ConcurrentHashMap()
+    val orderDataStore: Map<Long, Collection<Order>> = ConcurrentHashMap()
 
+    @MessageMapping("orders.{customerId}")
+    fun getOrders(@DestinationVariable customerId: Long): Flux<Order> {
+        return Flux.fromIterable(orderDataStore[customerId] ?: emptyList())
+    }
 
 }
