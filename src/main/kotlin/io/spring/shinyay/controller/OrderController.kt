74626c635f496f5fd7ecb2753c76581de04ed45b
shinyay <shinya.com@gmail.com>
2021-10-30 11:30:03 +0900

    Add sample data

--- src/main/kotlin/io/spring/shinyay/controller/OrderController.kt
+++ src/main/kotlin/io/spring/shinyay/controller/OrderController.kt
@@ -9,16 +9,24 @@ import reactor.core.publisher.Flux
 import java.util.concurrent.ConcurrentHashMap
 
 
-
-
 @Controller
 @ResponseBody
 class OrderController {
 
-    val orderDataStore: Map<Long, Collection<Order>> = ConcurrentHashMap()
+    private val orderDataStore: MutableMap<Int, Collection<Order>> = ConcurrentHashMap()
+
+    constructor() {
+        for (customerId in 1..8) {
+            val listOfOrders = ArrayList<Order>()
+            val max = (Math.random() * 100).toInt()
+
+            for (orderId in 1..max) listOfOrders.add(Order(orderId, customerId))
+            orderDataStore[customerId] = listOfOrders
+        }
+    }
 
     @MessageMapping("orders.{customerId}")
-    fun getOrders(@DestinationVariable customerId: Long): Flux<Order> {
+    fun getOrders(@DestinationVariable customerId: Int): Flux<Order> {
         return Flux.fromIterable(orderDataStore[customerId] ?: emptyList())
     }
 
